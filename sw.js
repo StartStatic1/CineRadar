69baff882f4d6bafceb6ee527d818f622138ec1f
StartStatic1 <startstatic@outlook.com>
2026-05-20 11:31:06 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11031103
+    "zoneId": 11033751
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
