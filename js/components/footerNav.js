0db84a12094ab65f74e143d3c4c069d38b2f5a0c
StartStatic1 <startstatic@outlook.com>
2026-05-29 11:32:17 -0300

    Update footerNav.js

--- js/components/footerNav.js
+++ js/components/footerNav.js
@@ -6,8 +6,7 @@ const FooterNav = {
 
         const items = [
             { path: '#/home', icon: 'fa-home', label: 'Inicio' },
-            { path: '#/explore', icon: 'fa-compass', label: 'Explorar' },
-            { path: '#/iptv', icon: 'fa-tv', label: 'TV' },
+            { path: '#/explore', icon: 'fa-compass', label: 'Explorar' },           
             { path: '#/reels', icon: 'fa-film', label: 'Reels' },
             { path: '#/mylist', icon: 'fa-bookmark', label: 'Minha Lista' },
             { path: '#/search', icon: 'fa-search', label: 'Buscar' }
